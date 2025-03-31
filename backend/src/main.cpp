3596667b6e98635600b33728917e9c14b5017e9e
landiluigi746 <landiluigi865@gmail.com>
2025-03-31 02:36:17 +0200

    [init] First commit with backend Hello world!

---
+++ backend/src/main.cpp
@@ -0,0 +1,14 @@
+#include <httplib.h>
+
+int main()
+{
+    httplib::Server svr;
+
+    svr.Get("/", [&] (const httplib::Request& req, httplib::Response& res) {
+        res.set_content("Hello from shurlx!", "text/plain");
+    });
+
+    svr.listen("0.0.0.0", 8080);
+
+    return 0;
+}
